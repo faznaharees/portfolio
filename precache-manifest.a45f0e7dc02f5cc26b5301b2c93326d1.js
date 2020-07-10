6d84ac62825393d8f3ed75a2c67675d7a93c7800
Fazna <faznaharees@cet.ac.in>
2020-07-10 13:24:11 +0530

    Updates

--- precache-manifest.a45f0e7dc02f5cc26b5301b2c93326d1.js
+++ precache-manifest.a45f0e7dc02f5cc26b5301b2c93326d1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38a3433140cf5aa8e947b8e0c93d68dd",
+    "revision": "e21dc74cd7e7abcda6ea991230bf82a6",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "34269edcf8e3a8250008",
+    "revision": "f8e0bf99802aa8c09b12",
     "url": "/portfolio/static/css/main.3284b0e4.chunk.css"
   },
   {
-    "revision": "27eb3e16a7eca15ba0ab",
-    "url": "/portfolio/static/js/2.2ff0daee.chunk.js"
+    "revision": "7437e22059bc4b91388e",
+    "url": "/portfolio/static/js/2.87f2faa4.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/portfolio/static/js/2.2ff0daee.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.87f2faa4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "34269edcf8e3a8250008",
-    "url": "/portfolio/static/js/main.d6cf07aa.chunk.js"
+    "revision": "f8e0bf99802aa8c09b12",
+    "url": "/portfolio/static/js/main.49d78a07.chunk.js"
   },
   {
     "revision": "6bafe6fdb4893a0fd8e5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3ba4431abbc027a7903d59548a05267d.js"
+  "/portfolio/precache-manifest.a45f0e7dc02f5cc26b5301b2c93326d1.js"
 );
 
 self.addEventListener('message', (event) => {
